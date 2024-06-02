0ecce2c680fca28036450fd21afead00306aedf1
João Afonso <ferreiraafonsojoao@ua.pt>
2024-06-02 16:41:30 +0100

    missing code

--- main/main.c
+++ main/main.c
@@ -1,3 +1,4 @@
+// new main
 #include "driver/i2c_master.h"
 #include "esp_http_server.h"
 #include "esp_http_client.h"
@@ -12,19 +13,38 @@
 #include "cJSON.h"
 #include "wifi.h"
 #include "nvs.h"
+#include "driver/adc.h"
+// #include "driver/spi_master.h"
+#include "driver/gpio.h"
+// #include "esp_ota_ops.h"
+// #include "esp_https_ota.h"
+// #include "esp_pm.h"
 
 /**
  * @brief Declaration of global variables (SDA and SCL pins and CLK_SPEED)
- * 
  */
 #define SDA_PIN 2
 #define SCL_PIN 6
 #define CLK_SPEED 100000 // 100 kHz
 
+/**
+ * @brief Define the SPI configuration for the SD card
+ */
+#define SD_HOST SPI2_HOST
+#define SD_CS_PIN 3
+#define SD_MOSI_PIN 0
+#define SD_CLK_PIN 7
+#define SD_MISO_PIN 5
+
+/**
+ * @brief Define light sensor GPIO  
+*/
+#define LIGHT_SENSOR_GPIO 1
+
 static const char *TAG = "main";
 
 /**
- * @brief Read sensor task - Readx Temperature, Humidity and Pressure from the BME280 sensor
+ * @brief Read sensor task - Read Temperature, Humidity, and Pressure from the BME280 sensor
  * 
  * @param pvParameters 
  */
@@ -36,8 +56,7 @@ void read_sensor_task(void *pvParameters) {
         ESP_LOGE(TAG, "Failed to initialize sensor");
         vTaskDelete(NULL); 
     }
-    
-    // infinite loop to read sensor data
+
     while (1) {
         int32_t raw_temp = 0;
         int32_t raw_humidity = 0;
@@ -61,17 +80,35 @@ void read_sensor_task(void *pvParameters) {
             ESP_LOGI(TAG, "Temperature: %.2f°C", temperature);
             ESP_LOGI(TAG, "Humidity: %.2f%%", humidity);
             ESP_LOGI(TAG, "Pressure: %.2fhPa", pressure);
+
+            // Read the value from the light sensor
+            int light_sensor_value = adc1_get_raw(LIGHT_SENSOR_GPIO);
+            ESP_LOGI(TAG, "Light sensor value: %d", light_sensor_value);
+
+            if(light_sensor_value < 200) {
+                ESP_LOGI(TAG, "The environment is dark\n");
+            } else if(light_sensor_value < 1000) {
+                ESP_LOGI(TAG, "The environment is mid\n");
+            } else if(light_sensor_value < 2000) {
+                ESP_LOGI(TAG, "The environment is bright\n");
+            } else {
+                ESP_LOGI(TAG, "The environment is very bright\n");
+            }
+
         } else {
             ESP_LOGE(TAG, "Failed to read sensor data");
         }
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+
+    ESP_LOGI(TAG, "Sensor task completed");
+    vTaskDelete(NULL);
 }
+
 
 void app_main() {
     // Initialize NVS (Non-Volatile Storage)
-    //  https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/storage/nvs_flash.html
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
@@ -85,6 +122,10 @@ void app_main() {
         ESP_LOGE(TAG, "Failed to create sensor data semaphore");
         return;
     }
+
+    // setup light sensor
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(LIGHT_SENSOR_GPIO, ADC_ATTEN_DB_11);
 
     // Initialize Wi-Fi connection
     wifi_init_station();
